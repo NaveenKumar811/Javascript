d010a08046936b3e2d8785beb22ae727d0442997
Naveen Kumar <146078159+NaveenKumar811@users.noreply.github.com>
2024-01-06 21:34:59 +0530

    Add files via upload

---
+++ Javascript/trail.js
@@ -0,0 +1,6 @@
+var name = prompt("Enter name: ")
+var age = prompt("Enter age: ")
+if(age > 18)
+alert("Hi " + name + ". You are eligible to vote")
+else
+alert("Sorry " + name + ". You are not eligible to vote !")
